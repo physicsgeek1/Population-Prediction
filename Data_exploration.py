644d02e2213c51660adaaf0e4ae86b4b5bbc5433
Stephen Ordway <stephenordway@Stephens-MBP.attlocal.net>
2022-01-28 22:05:07 -0600

    Add existing file

--- Data_exploration.py
+++ Data_exploration.py
@@ -1,3 +1,5 @@
+## The following code is used to identify the required weather data for analysisand then gather that information from NOAA
+
 import pandas as pd
 import numpy as np
 import requests
@@ -5,7 +7,7 @@ import datetime
 import os
 import sys
 
-
+# Function to grab weather data from NOAA through online API
 def get_weather(stationid, begin_date, end_date, mytoken, base_url):
     token = {'token': mytoken}
 
@@ -36,11 +38,15 @@ def get_weather(stationid, begin_date, end_date, mytoken, base_url):
 
 if __name__ == '__main__':
     pd.set_option('display.max_columns', None)
+    
+    #Initialization to use to activate pull code
+    grab_weather = 0
+    weather_dblchk = 0
 
     # reading in the files and appending all data into one dataframe
-    df = pd.read_csv('../../Raw/US_Mammalia/Clean_population_record_for_select_Mammalia.csv')
-    df2 = pd.read_csv('../../Raw/US_Amphibia/Clean_population_record_for_select_Amphibia.csv')
-    df3 = pd.read_csv('../../Raw/US_Reptilia/Clean_population_record_for_select_Reptilia.csv')
+    df = pd.read_csv('Clean_population_record_for_select_Mammalia.csv')
+    df2 = pd.read_csv('Clean_population_record_for_select_Amphibia.csv')
+    df3 = pd.read_csv('Clean_population_record_for_select_Reptilia.csv')
 
     df = df.append(df2)
     df = df.append(df3)
@@ -69,16 +75,12 @@ if __name__ == '__main__':
             col_list.append(col_name)
 
     df_wide.columns = col_list
-    # print('df_wide',df_wide.shape, '\n', df_wide.head())
     df_long = pd.wide_to_long(df_wide, stubnames='pop', i=['id'],
                               j='year').reset_index()
-    # print('df_long',df_long.shape, '\n', df_long.head())
     # recombining the data together to create a clean df
     df_clean = pd.merge(df_wide2, df_long, on='id', how='inner')
     df_clean = df_clean[df_clean['pop'].notnull()]
-    # print('df_clean', df_clean.shape, '\n', df_clean.head())
-    # print(df_clean.head())
-    # print(df_clean.shape)
+
 
     # uncomment if needed: here are a couple of quick ways to check number of data point and unique lat long combos.
     # df_latlong = df_clean[['year', 'Decimal Latitude', 'Decimal Longitude']]
@@ -91,33 +93,27 @@ if __name__ == '__main__':
 
     # pull all of the columns from latlong needed to match to a station_id.
     df_latlong = df_clean[['id', 'year', 'Decimal Latitude', 'Decimal Longitude']]
-    # print(df_latlong.shape)
 
     # calculate the min and max of the years that the study has data
     result = df_latlong.groupby('id').agg({'year': ['min', 'max']})
     result.columns = result.columns.map('_'.join)
     result = result.reset_index()
-    # print(result.head())
 
     #attaches min and max years to df_latlong
     df_latlong = pd.merge(df_latlong, result, on='id', how='left')
-    # print(df_latlong.shape)
-    # print(df_latlong.head())
 
     # creates a list with id# lat and long as well as min and max year to search and pull necessary weather data
     df_unilatlong = df_latlong.drop(columns=['year'])
     df_unilatlong = df_unilatlong.drop_duplicates()
     df_unilatlong = df_unilatlong.reset_index()
-    print('df_unilatlong', df_unilatlong.shape, '\n', df_unilatlong.head())
 
     # create the station_id dataframe in a way that it can be searched and used.
-    stations = pd.read_csv('../../FIPS/StationListUS.csv')
+    stations = pd.read_csv('StationListUS.csv')
     stations['mindate'] = pd.to_datetime(stations['mindate'], format='%Y-%m-%d')
     stations['maxdate'] = pd.to_datetime(stations['maxdate'], format='%Y-%m-%d')
     stations['minyear'] = stations['mindate'].dt.year
     stations['maxyear'] = stations['maxdate'].dt.year
     stations.rename(columns={'id': 'station_id'}, inplace=True)
-    # print('stations', stations.shape, '\n', stations.head())
 
     # initialize some arrays so the for loop is more manageable
     ids = df_unilatlong['id']
@@ -127,6 +123,7 @@ if __name__ == '__main__':
     maxs = df_unilatlong['year_max']
     station_id = pd.DataFrame(columns=['station_id'])
 
+    # the following for loop preapres list of stations to grab weather from with all necessary look up information
     for i in range(0, len(lats)):
         idloop = ids[i]
         latpop = lats[i]
@@ -149,120 +146,113 @@ if __name__ == '__main__':
         station_id.loc[i, 'station_id'] = stationdatesort.at[j, 'station_id']
 
     df_unilatlong['Station_ID'] = station_id['station_id']
-    print('df_unilatlong', df_unilatlong.shape, '\n', df_unilatlong.head())
 
-    ################# The Code to pull all of the weather data from the API is in  the condenced comment here.
-    # weather_data = pd.DataFrame()
-    # for k in range(0, len(df_unilatlong)):
-    #     stationid = df_unilatlong.at[k, 'Station_ID']
-    #     datasetid = 'GSOY'
-    #     begindate = df_unilatlong.at[k, 'year_min']
-    #     enddate = df_unilatlong.at[k, 'year_max']
-    #     mytoken = 'iWcBIsMUzseLjhZBmEnQGzxKAhyHMjCh'
-    #     if stationid == '':
-    #         continue
-    #     base_url = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GSOY'
-    #     for l in range(begindate, enddate):
-    #         begin_date = l
-    #         end_date = l + 1
-    #         temp = get_weather(stationid, begin_date, end_date, mytoken, base_url)
-    #         weather_data = weather_data.append(temp, ignore_index=True, sort=True)
-    #
-    #         print(k, stationid, 'weather_data shape', weather_data.shape)
-    #
-    #     # print('temp', temp.head())
-    # print(weather_data.head())
-    # print('weather_data shape', weather_data.shape)
-    # output_file_name = '../../Weather_api/WeatherData.csv'
-    # weather_data.to_csv(output_file_name, index=False)
+    ################# The Code to pull all of the weather data from the API.
+    if grab_weather == 1:
+        weather_data = pd.DataFrame()
+        for k in range(0, len(df_unilatlong)):
+            stationid = df_unilatlong.at[k, 'Station_ID']
+            datasetid = 'GSOY'
+            begindate = df_unilatlong.at[k, 'year_min']
+            enddate = df_unilatlong.at[k, 'year_max']
+            mytoken = 'iWcBIsMUzseLjhZBmEnQGzxKAhyHMjCh'
+            if stationid == '':
+                continue
+            base_url = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GSOY'
+            for l in range(begindate, enddate):
+                begin_date = l
+                end_date = l + 1
+                temp = get_weather(stationid, begin_date, end_date, mytoken, base_url)
+                weather_data = weather_data.append(temp, ignore_index=True, sort=True)
+        
+                print(k, stationid, 'weather_data shape', weather_data.shape)
+        
+            # print('temp', temp.head())
+        print(weather_data.head())
+        print('weather_data shape', weather_data.shape)
+        output_file_name = '../../Weather_api/WeatherData.csv'
+        weather_data.to_csv(output_file_name, index=False)
 
-    #### Read in CSV of weather data and convert from long format to wide format for all of the measurables/attributes
-
-    Weatherdata = pd.read_csv('../../Weather_api/WeatherData.csv')
-    print('Weatherdata', Weatherdata.shape)
-    Weatherdata2 = pd.read_csv('../../Weather_api/WeatherData2.csv')
-    print('Weatherdata2', Weatherdata2.shape)
+    # Read in CSV of weather data and convert from long format to wide format for all of the measurables/attributes
+    Weatherdata = pd.read_csv('WeatherData.csv')
+    Weatherdata2 = pd.read_csv('WeatherData2.csv')
     Weatherdata = Weatherdata.append(Weatherdata2)
-    print('Weatherdata', Weatherdata.shape)
     Weatherdata = Weatherdata.drop(columns=['attributes'])
     Weatherdata = Weatherdata.drop_duplicates()
     Wdata_wide = Weatherdata.pivot_table(index = ['station','date'], columns = 'datatype', values = 'value').reset_index()
     Wdata_wide['date'] = pd.to_datetime(Wdata_wide['date'])
     Wdata_wide['year'] = Wdata_wide['date'].dt.year
     Wdata_wide = Wdata_wide.drop(columns=['date'])
-    print('Weatherdata', Weatherdata.shape, '\n', Weatherdata.head())
-    print('Wdata_wide', Wdata_wide.shape, '\n', Wdata_wide.head())
 
-    ### connect the Station ID data to the sutdy id so that it can be merged with pop data in df_clean
+    # Connect the Station ID data to the study id so that it can be merged with pop data in df_clean
     joined = pd.merge(Wdata_wide, df_unilatlong[['id', 'Station_ID']], left_on='station', right_on='Station_ID')
     joined = joined.drop(columns=['station'])
-    print('joined', joined.shape, '\n', joined.head())
     Final = pd.merge(df_clean,joined, on=['id', 'year'], how='left')
-    print('Final', Final.shape, '\n', Final.head())
-    output_file_name = '../../Weather_api/Final2.csv'
+    output_file_name = 'Final2.csv'
     Final.to_csv(output_file_name, index=False)
-
+    
+    # Identify any pop data without weather info for second API call in case. Missing info saved as WeatherData2 below.
     Missing = Final[['id', 'Decimal Latitude', 'Decimal Longitude', 'year', 'Station_ID']]
     Missing = Missing[Missing['Station_ID'].isnull()]
-    print('Missing', Missing.shape, '\n', Missing.head())
 
-    ### Find the population values that do not have any associated weather data. Some will have to be dropped if a station is not close enough.
-    # Missing = Final[['id', 'Decimal Latitude', 'Decimal Longitude', 'year', 'Station_ID']]
-    # Missing = Missing[Missing['Station_ID'].isnull()]
-    # Missing = Missing.reset_index()
-    # print('Missing', Missing.shape, '\n', Missing.head())
-    #
-    # M_ids = Missing['id']
-    # M_lats = Missing['Decimal Latitude']
-    # M_longs = Missing['Decimal Longitude']
-    # M_years = Missing['year']
-    # station_id2 = pd.DataFrame(columns=['station_id'])
-    #
-    # #gather and attach all of the station id info to the missing data
-    # for i in range(0, len(M_lats)):
-    #     idloop2 = M_ids[i]
-    #     latpop2 = M_lats[i]
-    #     longpop2 = M_longs[i]
-    #     year = M_years[i]
-    #     stationds = stations.loc[stations['minyear'] <= year]
-    #     stationds = stationds.loc[stationds['maxyear'] >= year]
-    #     stationds = stationds.loc[stationds['datacoverage'] >= 0.7]
-    #     stationds = stationds[abs(stationds['latitude']).between(abs(latpop2) - 5, abs(latpop2) + 5)]
-    #     stationds = stationds[abs(stationds['longitude']).between(abs(longpop2) - 5, abs(longpop2) + 5)]
-    #     if len(stationds) == 0:
-    #         print('no data for '+ str(i) + ' lat= ' + str(latpop2) + ' long= ' + str(longpop2) + ' id= ' + str(idloop2))
-    #         station_id.loc[i, 'station_id'] = ''  # np.nan
-    #         continue
-    #
-    #     stationds['Distance'] = np.sqrt((stationds['latitude'] - latpop) ** 2 +
-    #                                           (stationds['longitude'] - longpop) ** 2)
-    #     j = stationds['Distance'].idxmin()
-    #     station_id2.loc[i, 'station_id'] = stationds.at[j, 'station_id']
-    #
-    # Missing['Station_ID'] = station_id2['station_id']
-    # print('Missing', Missing.shape, '\n', Missing.head())
-    # Missing = Missing[Missing['Station_ID'].notnull()].reset_index()
-    # print('Missing', Missing.shape, '\n', Missing.head())
-    #
-    # weather_data = pd.DataFrame()
-    # for k in range(0, len(Missing)):
-    #     stationid = Missing.at[k, 'Station_ID']
-    #     datasetid = 'GSOY'
-    #     begindate = int(Missing.at[k, 'year'])
-    #     enddate = begindate + 1
-    #     mytoken = 'iWcBIsMUzseLjhZBmEnQGzxKAhyHMjCh'
-    #     base_url = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GSOY'
-    #     # for l in range(begindate, enddate):
-    #     #     begin_date = l
-    #     #     end_date = l + 1
-    #     temp = get_weather(stationid, begindate, enddate, mytoken, base_url)
-    #     weather_data = weather_data.append(temp, ignore_index=True, sort=True)
-    #
-    #     print(k, stationid, 'weather_data shape', weather_data.shape)
-    #
-    #     # print('temp', temp.head())
-    # print(weather_data.head())
-    # print('weather_data shape', weather_data.shape)
-    # output_file_name = '../../Weather_api/WeatherData2.csv'
-    # weather_data.to_csv(output_file_name, index=False)
+    # Find the population values that do not have any associated weather data. Some will have to be dropped if a station is not close enough.
+    if weather_dblchk == 1:
+        Missing = Final[['id', 'Decimal Latitude', 'Decimal Longitude', 'year', 'Station_ID']]
+        Missing = Missing[Missing['Station_ID'].isnull()]
+        Missing = Missing.reset_index()
+        print('Missing', Missing.shape, '\n', Missing.head())
+        
+        M_ids = Missing['id']
+        M_lats = Missing['Decimal Latitude']
+        M_longs = Missing['Decimal Longitude']
+        M_years = Missing['year']
+        station_id2 = pd.DataFrame(columns=['station_id'])
+        
+        #gather and attach all of the station id info to the missing data
+        for i in range(0, len(M_lats)):
+            idloop2 = M_ids[i]
+            latpop2 = M_lats[i]
+            longpop2 = M_longs[i]
+            year = M_years[i]
+            stationds = stations.loc[stations['minyear'] <= year]
+            stationds = stationds.loc[stationds['maxyear'] >= year]
+            stationds = stationds.loc[stationds['datacoverage'] >= 0.7]
+            stationds = stationds[abs(stationds['latitude']).between(abs(latpop2) - 5, abs(latpop2) + 5)]
+            stationds = stationds[abs(stationds['longitude']).between(abs(longpop2) - 5, abs(longpop2) + 5)]
+            if len(stationds) == 0:
+                print('no data for '+ str(i) + ' lat= ' + str(latpop2) + ' long= ' + str(longpop2) + ' id= ' + str(idloop2))
+                station_id.loc[i, 'station_id'] = ''  # np.nan
+                continue
+        
+            stationds['Distance'] = np.sqrt((stationds['latitude'] - latpop) ** 2 +
+                                                  (stationds['longitude'] - longpop) ** 2)
+            j = stationds['Distance'].idxmin()
+            station_id2.loc[i, 'station_id'] = stationds.at[j, 'station_id']
+        
+        Missing['Station_ID'] = station_id2['station_id']
+        print('Missing', Missing.shape, '\n', Missing.head())
+        Missing = Missing[Missing['Station_ID'].notnull()].reset_index()
+        print('Missing', Missing.shape, '\n', Missing.head())
+        
+        weather_data = pd.DataFrame()
+        for k in range(0, len(Missing)):
+            stationid = Missing.at[k, 'Station_ID']
+            datasetid = 'GSOY'
+            begindate = int(Missing.at[k, 'year'])
+            enddate = begindate + 1
+            mytoken = 'iWcBIsMUzseLjhZBmEnQGzxKAhyHMjCh'
+            base_url = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GSOY'
+            # for l in range(begindate, enddate):
+            #     begin_date = l
+            #     end_date = l + 1
+            temp = get_weather(stationid, begindate, enddate, mytoken, base_url)
+            weather_data = weather_data.append(temp, ignore_index=True, sort=True)
+        
+            print(k, stationid, 'weather_data shape', weather_data.shape)
+        
+            # print('temp', temp.head())
+        print(weather_data.head())
+        print('weather_data shape', weather_data.shape)
+        output_file_name = '../../Weather_api/WeatherData2.csv'
+        weather_data.to_csv(output_file_name, index=False)
 
--- make_labels.py
+++ make_labels.py
@@ -1,6 +1,12 @@
+# This script takes the consilidated data from Data_gathering.py and creates datasets
+# based on percentage fill of measured weather parameters available. For all missing data,
+# the missing attribute is approximated with the mean value of the available measured
+# attributes over all years at that location. Then any training examples missing any 
+# information are removed. Lastly, the datasets are exported as .csv to be used with 
+# machine learning code.
+
 import pandas as pd
 import numpy as np
-#import sklearn
 from sklearn.preprocessing import StandardScaler 
 from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
@@ -10,91 +16,71 @@ if __name__ == '__main__':
     plt.style.use('seaborn-white')
     pd.set_option('display.max_columns', None)
 
-    df = pd.read_csv('../../Weather_api/Final2.csv')
+    # reads in .csv containing population and corresponding weather data
+    df = pd.read_csv('Final2.csv')
     df = df[df['Station_ID'].notnull()].sort_values(by=['id','year'])
-    #df['popchange'] = df['pop'].diff()
-    #df['popchange'] = df['popchange'].fillna(df.groupby('id')['popchange'].transform('mean'))
     
+    # find unique study id's and initialize empty sets needed for scaling
     b = np.array([df['id']]).transpose()
     pop = np.array([df['pop']]).transpose()
-    #popchange = np.array([df['popchange']]).transpose()
     uid = np.unique(b)
     idpop = np.append(b,pop, axis=1)
-    #idpop = np.append(idpop,popchange, axis=1)
     stdscale = []
     mmscale = []
-#    stdchange = []
-#    mmchange = []
     ss = StandardScaler()
     mm = MinMaxScaler()
     
+    # the following for loop scales each study's measured populations as eithere
+    # a standard scale (stdscale) or a minmix scalee (mmscale).
     for ids in uid:
         temp = idpop[idpop[:,0]==ids]
         temp2 = temp[:,1]
         #temp3 = temp[:,2]
         if len(stdscale) == 0:
             stdscale = ss.fit_transform(temp2.reshape(-1,1))
-            #stdchange = ss.fit_transform(temp3.reshape(-1,1))
         else:
             ssscale = ss.fit_transform(temp2.reshape(-1,1))
             stdscale = np.append(stdscale, ssscale, axis=0)
-            #sschange = ss.fit_transform(temp3.reshape(-1,1))
-            #stdchange = np.append(stdchange, sschange, axis=0)
             del ssscale
-#            del sschange
+
             
         if len(mmscale) == 0:
             mmscale = mm.fit_transform(temp2.reshape(-1,1))
-            #mmchange = mm.fit_transform(temp3.reshape(-1,1))
         else:
             mscale = mm.fit_transform(temp2.reshape(-1,1))
             mmscale = np.append(mmscale, mscale, axis=0)
-            #mchange = mm.fit_transform(temp3.reshape(-1,1))
-            #mmchange = np.append(mmchange, mchange, axis=0)            
             del mscale  
-#            del mchange
-#        del temp
-#        del temp2
-#        del temp3
-        
+    
+    # Assign stdscale and mmscale to df.
     df['stdscale'] = stdscale
     df['mmscale'] = mmscale
-
-#    df['stdchange'] = stdchange
-#    df['mmchange'] = mmchange
-
-    df1 = df.loc[:, df.isnull().mean() <= 0.3]  
-    X = df1.isnull().mean()
-
-    df1['TMAX'] = df1['TMAX'].fillna(df1.groupby('Station_ID')['TMAX'].transform('mean'))  
-    df1['DP01'] = df1['DP01'].fillna(df1.groupby('Station_ID')['DP01'].transform('mean'))  
-    df1['DP10'] = df1['DP10'].fillna(df1.groupby('Station_ID')['DP10'].transform('mean'))  
-    df1['DP1X'] = df1['DP1X'].fillna(df1.groupby('Station_ID')['DP1X'].transform('mean'))  
-    df1['DSNW'] = df1['DSNW'].fillna(df1.groupby('Station_ID')['DSNW'].transform('mean'))  
-    df1['DT00'] = df1['DT00'].fillna(df1.groupby('Station_ID')['DT00'].transform('mean'))  
-    df1['DT32'] = df1['DT32'].fillna(df1.groupby('Station_ID')['DT32'].transform('mean'))  
-    df1['DX70'] = df1['DX70'].fillna(df1.groupby('Station_ID')['DX70'].transform('mean'))  
-    df1['DX90'] = df1['DX90'].fillna(df1.groupby('Station_ID')['DX90'].transform('mean'))  
-    df1['EMNT'] = df1['EMNT'].fillna(df1.groupby('Station_ID')['EMNT'].transform('mean'))  
-    df1['EMSN'] = df1['EMSN'].fillna(df1.groupby('Station_ID')['EMSN'].transform('mean'))  
-    df1['EMXP'] = df1['EMXP'].fillna(df1.groupby('Station_ID')['EMXP'].transform('mean'))  
-    df1['EMXT'] = df1['EMXT'].fillna(df1.groupby('Station_ID')['EMXT'].transform('mean'))  
-    df1['PRCP'] = df1['PRCP'].fillna(df1.groupby('Station_ID')['PRCP'].transform('mean'))  
-    df1['SNOW'] = df1['SNOW'].fillna(df1.groupby('Station_ID')['SNOW'].transform('mean'))  
-    df1['TMIN'] = df1['TMIN'].fillna(df1.groupby('Station_ID')['TMIN'].transform('mean'))  
-
+    
+    
+    # df1 used to identify all features with <70% fill. Then any missing values 
+    # are replaced with locational average of available data for the attribute.
+    # Final result saved in df2.
+    df1 = df.loc[:, df.isnull().mean() <= 0.3]
+    meanlbl_df1 = ['TMAX', 'DP01', 'DP10', 'DP1X', 'DSNW', 'DT00', 'DT32', 'DX70', 
+                  'DX90', 'EMNT', 'EMSN', 'EMXP', 'EMXT', 'PRCP', 'SNOW', 'TMIN']
+    
+    for i in meanlbl_df1:
+        df1[i] = df1[i].fillna(df1.groupby('Station_ID')[i].transform('mean'))
+   
     df2 = df1.dropna(axis = 0)
     
-    df3 = df.loc[:, df.isnull().mean() <= 0.2]  
-    Y = df3.isnull().mean()
-
-    df3['DP01'] = df3['DP01'].fillna(df1.groupby('Station_ID')['DP01'].transform('mean'))  
-    df3['DP10'] = df3['DP10'].fillna(df1.groupby('Station_ID')['DP10'].transform('mean'))  
-    df3['DP1X'] = df3['DP1X'].fillna(df1.groupby('Station_ID')['DP1X'].transform('mean'))  
-    df3['EMXP'] = df3['EMXP'].fillna(df1.groupby('Station_ID')['EMXP'].transform('mean'))  
-    df3['PRCP'] = df3['PRCP'].fillna(df1.groupby('Station_ID')['PRCP'].transform('mean')) 
-    df4 = df3.dropna(axis = 0)
     
+    # df3 used to identify all features with <80% fill. Then any missing values 
+    # are replaced with locational average of available data for the attribute.
+    # Final result saved in df4.    
+    df3 = df.loc[:, df.isnull().mean() <= 0.2]  
+    meanlbl_df2 = ['DP01', 'DP10', 'DP1X', 'EMXP', 'PRCP']
+    
+    for i in meanlbl_df2:
+        df3[i] = df3[i].fillna(df1.groupby('Station_ID')[i].transform('mean'))
+
+    df4 = df3.dropna(axis = 0)
+
+
     #PLOTS FOR data70
     plt.figure(1)
     plt.scatter(df2['year'], df2['pop'])
@@ -130,16 +116,7 @@ if __name__ == '__main__':
     plt.title('Scaled Populations - Dataset 1') 
     plt.savefig('popscaled80.png')
     
-#    fig, axs = plt.subplots(1,2)
-#    fig.suptitle('Populations')
-#    axs[0,0].plot(df2['year'], df2['pop'])
-#    axs[0,0].set_title('Populations70')
-#    axs[0,1].plot(df4['year'], df4['pop'])
-#    axs[0,1].set_title('Populations80')
-#    plt.xlabel('Year')
-#    plt.ylabel('Scaled Population')
-
-
+    
     ######### groupby id, calculate percent change in pop, and remove any data that isnt an incremental year
     grouped = df2.groupby(['id']).apply(lambda x: x.sort_values(by=['year']))
     grouped['yeardiff'] = grouped['year'].diff()
@@ -149,9 +126,8 @@ if __name__ == '__main__':
     df_out = grouped.reset_index(drop=True)
     df_out = df_out[df_out['yeardiff'] == 1]  # drops datapoints that are skip years
 
-    output_file_name = '../../processed/data70.csv'
+    output_file_name = 'data70.csv'
     #df_out.to_csv(output_file_name, index=False)
-    print(df_out.shape)
 
     grouped2 = df4.groupby(['id']).apply(lambda x: x.sort_values(by=['year']))
     grouped2['yeardiff'] = grouped2['year'].diff()
@@ -161,17 +137,7 @@ if __name__ == '__main__':
     df_out2 = grouped2.reset_index(drop=True)
     df_out2 = df_out2[df_out2['yeardiff'] == 1]  # drops datapoints that are skip years
 
-    output_file_name = '../../processed/data80.csv'
+    output_file_name = 'data80.csv'
     #df_out2.to_csv(output_file_name, index=False)
-    print(df_out2.shape)
 
-    #percent of filled feature values by year
-   # y = df.loc[:, df.isnull().mean() <= 0.3]
-    y = df.drop(['Class','Order','Family','Genus','Species','Common Name','Region',
-              'Are coordinates for specific location?','system','biome','realm',
-              'Native','Alien','Invasive','Units','Sampling method','Data transformed'], axis=1)
-    y = y.drop(['Station_ID'], axis=1)
-    pctfill = y.groupby('year').apply(lambda x: x.notnull().sum()/len(x)*100)
 
-#df_scaled = pd.DataFrame(ss.fit_transform(df),columns = df.columns)
-    #scaler.fit_transform(dfTest[['A','B']].values)
